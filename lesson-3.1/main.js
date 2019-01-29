ba37d52b1498a598fbfbfa49f27d48d0639b7d16
TuMaN-1122 <tuman-1122@yandex.ru>
2019-01-29 23:36:12 +0300

    урок №4 начало

---
+++ lesson-3.1/main.js
@@ -0,0 +1,87 @@
+'use strict'
+
+let money = +prompt('Ваш бюджет на месяц?', '');
+let time = prompt('Введите дату в формате YYYY-MM-DD', '');
+
+let appData = {
+	bydget: money,
+	timeDate: time,
+	expenses: {},
+	optionalExpenses: {},
+	income: [],
+	savings: false
+};
+
+/*Делаем проверку что переменная -a- является строкой
+ и переменные -а- и -b- не была отмена при вызове в prompt, 
+эти переменные не были пустыми строками 
+и переменная -а- не превышала больше 50 символов
+*/
+
+for(let i = 0; i < 2; i++){
+	let a = prompt('Введите обязательную статью расходов в этом месяце', ''),
+		b = prompt('Во сколько обойдется?', '');
+
+	if( (typeof(a)) === 'string' && (typeof(a)) != null && (typeof(b)) != null && a != '' && b != '' && a.length < 50){
+		
+		console.log('done');
+		appData.expenses[a] = b;
+	} else{
+		i = i - 1;
+		console.log('Ошибка при вводе данных!');
+	}
+}
+
+// Цикл while
+/*
+let i = 0;
+while (i < 2) {
+	let a = prompt('Введите обязательную статью расходов в этом месяце', ''),
+		b = prompt('Во сколько обойдется?', '');
+
+	if( (typeof(a)) === 'string' && (typeof(a)) != null && (typeof(b)) != null && a != '' && b != '' && a.length < 50){
+		
+		console.log('done');
+		appData.expenses[a] = b;
+	} else{
+		i = i - 1;
+		console.log('Ошибка при вводе данных!');
+	}
+	i++;
+}
+*/
+
+
+// Цикл do while
+/*
+let i = 0;
+do{
+	let a = prompt('Введите обязательную статью расходов в этом месяце', ''),
+		b = prompt('Во сколько обойдется?', '');
+
+	if( (typeof(a)) === 'string' && (typeof(a)) != null && (typeof(b)) != null && a != '' && b != '' && a.length < 50){
+		console.log('done');
+		appData.expenses[a] = b;
+	} else{
+		i = i - 1;
+		console.log('Ошибка при вводе данных!');
+	}
+	i++;
+}while( i < 2);
+*/
+
+appData.moneyPerDay = appData.bydget / 30;
+
+alert('Ежедневный бюджет ' + appData.moneyPerDay);
+
+if(appData.moneyPerDay < 100){
+	console.log('Минимальный уровень достатка');
+} else if(appData.moneyPerDay > 100 && appData.moneyPerDay < 2000){
+	console.log('Средний уровень достатка');
+} else if(appData.moneyPerDay > 2000){
+	console.log('Высокий уровень достатка');
+} else(
+	console.log('Произошла ошибка')
+);
+console.log(appData);
+
