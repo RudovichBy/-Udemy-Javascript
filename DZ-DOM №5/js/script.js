a79799909b48ca6c7af3ed2d3fde9364bc2cd8bd
TuMaN-1122 <tuman-1122@yandex.ru>
2019-06-05 00:14:01 +0300

    lesson-6

---
+++ DZ-DOM №5/js/script.js
@@ -0,0 +1,31 @@
+'use strict'
+
+let li = document.createElement('li');
+let menu = document.querySelector('.menu');
+let menuItem = document.getElementsByClassName('menu-item');
+let title = document.getElementById('title');
+let adv = document.querySelector('.adv');
+let body = document.createElement('body');
+let prompt = document.querySelector('.prompt');
+
+
+// Восстановить порядок в меню
+menu.insertBefore(menuItem[2], menuItem[1]);
+
+// добавить пятый пункт
+li.classList.add('menu-item');
+li.textContent = 'Пятый пункт';
+menu.appendChild(li);
+
+// Заменить картинку заднего фона на другую из папки img
+document.body.style.backgroundImage = 'url(img/apple_true.jpg)';
+
+// Поменять заголовок, добавить слово "подлинную" ( Получится - "Мы продаем только подлинную технику Apple")
+title.innerHTML = 'Мы продаем только подлинную технику Apple';
+
+// Удалить рекламу со страницы
+adv.remove();
+
+ // Спросить у пользователя отношение к технике apple и записать ответ в блок на странице с id "prompt"
+let q = prompt('Ваше отношение  к технике Apple', '');
+console.log(q)
