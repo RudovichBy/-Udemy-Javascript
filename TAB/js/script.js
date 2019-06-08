72229a7ee73f8217547fbaa9f5ea1e37f23b59b4
TuMaN-1122 <tuman-1122@yandex.ru>
2019-06-08 15:43:20 +0300

    time less

--- TAB/js/script.js
+++ TAB/js/script.js
@@ -1,6 +1,6 @@
 window.addEventListener('DOMContentLoaded', function() {
 	'use strict';
-	
+// ===========================Табы===========================
 	let tab =  document.querySelectorAll('.info-header-tab'),
 		info = document.querySelector('.info-header'),
 		tabContent = document.querySelectorAll('.info-tabcontent');
@@ -33,4 +33,50 @@ window.addEventListener('DOMContentLoaded', function() {
 			}
 		}
 	}) 
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// =======================Таймер=======================
+
+	let deadline = '2019-06-21'; //Дата оканчания 
+
+	function getTimeRemaining(endtime) {
+		let t = Date.parse(endtime) - Date.parse(new Date()), //Определяем сколько времени осталось (дата оканчания таймера минус ныняшняя дата)
+		seconds = Math.floor((t/1000) % 60), //определяем количество оставшихся секунд
+		minutes = Math.floor((t/1000/60) % 60), //определяем количество оставшихся минут
+		hours = Math.floor((t/(1000*60*60))); //определяем количество оставшихся часов
+
+		/*
+		// Если нужнен таймер с количеством дней
+		hours = Math.floor((t/1000/60/60) % 24),
+		days = Math.floor((t/(1000*60*60*24)));
+		*/
+		return {
+			'total' : t,
+			'hours' : hours,
+			'minutes' : minutes,
+			'seconds' : seconds
+		};
+	}
+
+	function setClock(id, endtime) {
+		let timer = document.getElementById(id),
+			hours = timer.querySelector('.hours'),
+			minutes = timer.querySelector('.minutes'),
+			seconds = timer.querySelector('.seconds'),
+			timeInterval = setInterval(updateClock, 1000);
+
+		function updateClock() {
+			let t = getTimeRemaining(endtime);
+			hours.textContent = t.hours;
+			minutes.textContent = t.minutes;
+			seconds.textContent = t.seconds;
+
+			if(t.total <= 0) {
+				clearInterval(timeInterval);
+			}
+		}
+	}
+	setClock('timer', deadline);
+
+
 });
